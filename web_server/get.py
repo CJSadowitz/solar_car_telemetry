678971482eac52e6144391922d0c844a136334ae
colin sadowitz <sadowitzc@gmail.com>
2025-06-05 13:28:28 -0400

    Setup get-recent from database

---
+++ web_server/get.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def get_recent_entries():
+	conn = sqlite3.connect("../database.db")
+	cursor = conn.cursor()
+
+	cursor.execute("SELECT table_name FROM can_devices")
+	tables = cursor.fetchall()
+
+	recent_data = {}
+	for (table_name, ) in tables:
+		cursor.execute(f"SELECT raw FROM {table_name} ORDER BY timestamp DESC LIMIT 1")
+		data = cursor.fetchone()
+		if data != None:
+			recent_data[table_name] = str(data[0])
+
+	return recent_data
+
+if __name__ == "__main__":
+	get_recent_entries()
