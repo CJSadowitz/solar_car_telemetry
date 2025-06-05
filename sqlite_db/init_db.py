7f7820cec47de13d18f00e590a2bf3407c25b03b
colin sadowitz <sadowitzc@gmail.com>
2025-06-05 12:33:54 -0400

    Fixed table names and changed id's to lowercase as expected from can data

--- sqlite_db/init_db.py
+++ sqlite_db/init_db.py
@@ -2,22 +2,22 @@ import sqlite3
 
 def init_database():
 	mppt1_tables = ["mppt1_input", "mppt1_output", "mppt1_temp", "mppt1_aux_power", "mppt1_limits", "mppt1_status", "mppt1_power_connector", "mppt1_mode", "mppt1_max_output_voltage", "mppt1_max_input_current"]
-	mppt1_ids = ["600", "601", "602", "603", "604", "605", "606", "608", "60A", "60B"]
+	mppt1_ids = ["600", "601", "602", "603", "604", "605", "606", "608", "60a", "60b"]
 
 	mppt2_tables = ["mppt2_input", "mppt2_output", "mppt2_temp", "mppt2_aux_power", "mppt2_limits", "mppt2_status", "mppt2_power_connector", "mppt2_mode", "mppt2_max_output_voltage", "mppt2_max_input_current"]
-	mppt2_ids = ["610", "611", "612", "613", "614", "615", "616", "618", "61A", "61B"]
+	mppt2_ids = ["610", "611", "612", "613", "614", "615", "616", "618", "61a", "61b"]
 
-	cmu_tables = ["cmu_300", "cmu_301", "cmu_302", "cmu_303", "cmu_304", "cmu_305", "cmu_306", "cmu_307", "cmu_308", "cmu_309", "cmu_30A", "cmu_30B", "cmu_30C"]
-	cmu_ids = ["300", "301", "302", "303", "304", "305", "306", "307", "308", "309", "30A", "30B", "30C"]
+	cmu_tables = ["cmu_301", "cmu_302", "cmu_303", "cmu_304", "cmu_305", "cmu_306", "cmu_307", "cmu_308", "cmu_309", "cmu_30A", "cmu_30B", "cmu_30C"]
+	cmu_ids = ["301", "302", "303", "304", "305", "306", "307", "308", "309", "30a", "30b", "30c"]
 
 	bmu_tables = ["bmu_heartbeat_sensor", "pack_state_of_charge", "pack_balance_state_of_charge", "charger_control_info", "precharge_status", "min_max_cell_voltage", "min_max_cell_temp", "battery_pack_info", "battery_pack_status", "battery_pack_fan_status", "battery_pack_extended_info"]
-	bmu_ids = ["300", "3F4", "3F5", "3F6", "3F7", "3F8", "3F9", "3FA", "3FB", "3FC", "3FD"]
+	bmu_ids = ["300", "3f4", "3f5", "3f6", "3f7", "3f8", "3f9", "3fa", "3fb", "3fc", "3fd"]
 
 	tables = mppt1_tables + mppt2_tables + cmu_tables + bmu_tables
 	ids = mppt1_ids + mppt2_ids + cmu_ids + bmu_ids
 
 	init_tables(tables, ids)
-	init_data(ids)
+	init_data(ids, tables)
 
 def init_tables(tables, can_ids):
 	conn = sqlite3.connect("../database.db")
@@ -27,7 +27,7 @@ def init_tables(tables, can_ids):
 	cursor.execute(f"""
 	CREATE TABLE IF NOT EXISTS can_devices (
 		can_id TEXT PRIMARY KEY,
-		description TEXT
+		table_name TEXT
 	);
 	""")
 
@@ -43,12 +43,11 @@ def init_tables(tables, can_ids):
 	conn.commit()
 	conn.close()
 
-def init_data(ids):
+def init_data(ids, names):
 	# Insert all data in the master table base
 	conn = sqlite3.connect("../database.db")
 	cursor = conn.cursor()
 
-	data = [(can_id, f"can device {can_id}") for can_id in ids]
-	cursor.executemany("INSERT OR IGNORE INTO can_devices (can_id, description) VALUES (?, ?);", data)
+	cursor.executemany("INSERT OR IGNORE INTO can_devices (can_id, table_name) VALUES (?, ?);", list(zip(ids, names)))
 	conn.commit()
 	conn.close()
