22d525fb9a9a1004eaace7c9743a3f91fe6d28b8
colin sadowitz <sadowitzc@gmail.com>
2025-06-08 15:19:36 -0400

    Added gps to startup script

---
+++ startup/child_scripts/gps.sh
@@ -0,0 +1,3 @@
+echo "gps script"
+source ../gps/venv/bin/activate
+python3 ../gps/main.py
--- startup/master_startup.sh
+++ startup/master_startup.sh
@@ -4,6 +4,7 @@ echo "Calling Child Scripts"
 bash child_scripts/database.sh
 bash child_scripts/can.sh &
 bash child_scripts/lte.sh &
+bash child_scripts/gps.sh &
 bash child_scripts/cloudflare.sh &
 bash child_scripts/profinity.sh &
 bash child_scripts/gui.sh &
