c4ffb9979c2ceafd7978b98452406f0f2ce3fe16
colin sadowitz <sadowitzc@gmail.com>
2025-06-08 15:27:15 -0400

    Added gps script

--- startup/kill_startup.sh
+++ startup/kill_startup.sh
@@ -1,5 +1,5 @@
 echo "Shutting down active scripts"
-for script in child_scripts/can.sh child_scripts/lte.sh child_scripts/cloudflare.sh child_scripts/profinity.sh child_scripts/gui.sh master_startup.sh child_scripts/database.sh; do
+for script in child_scripts/can.sh child_scripts/lte.sh child_scripts/cloudflare.sh child_scripts/profinity.sh child_scripts/gui.sh master_startup.sh child_scripts/database.sh child_scripts/gps.sh; do
 	PID=$(pgrep -f "$script")
 	if [ -n "$PID" ]; then
 		kill "$PID"
