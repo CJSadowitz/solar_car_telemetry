bed6a65e96016eee7e6fefef180880a28a7598fb
colin sadowitz <sadowitzc@gmail.com>
2025-06-09 09:38:43 -0400

    Added functions and checksum error check to keep loop going

--- gps/main.py
+++ gps/main.py
@@ -3,39 +3,45 @@ import pynmea2
 import write_db
 
 def main():
-	ser = None
-	try:
-		port = "/dev/ttyACM0"
-		ser = serial.Serial(port, 9600, timeout=1)
-	except Exception as e:
-		print ("MAIN::SERIAL_PORT::unknown exception", e)
+	ser = get_serial_port("/dev/ttyACM0")
+	if (ser == None):
 		return 1
 
+	output = 0
+	while output != 1:
+		output = loop_gps_lines(ser)
+
+	ser.close()
+
+def get_serial_port(port):
+	try:
+		ser = serial.Serial(port, 9600, timeout=1)
+		return ser
+	except Exception as e:
+		print ("GPS::MAIN::get_serial_port::exception:", e)
+		return None
+
+def loop_gps_lines(ser):
 	try:
 		lat, lon, alt = None, None, None
-		while True:
-			line = ser.readline().decode("utf-8").strip()
-			if line.startswith("$GNRMC"):
-				msg = pynmea2.parse(line)
-				lat = msg.latitude
-				lon = msg.longitude
+		line = ser.readline().decode("utf-8").strip()
+		msg = pynmea2.parse(line)
+		if line.startswith("$GNRMC"):
+			lat, lon = msg.latitude, msg.longitude
+		elif line.startswith("$GNGGA"):
+			alt = msg.altitude
+		if (lat != None and lon != None and alt != None):
+			write_db.save_message(lat, lon, alt)
+			lat, lon, alt = None, None, None
+			return 0
 
-			if line.startswith("$GNGGA"):
-				msg = pynmea2.parse(line)
-				alt = msg.altitude
-
-			if (lat != None and lon != None and alt != None):
-				write_db.save_message(lat, lon, alt)
-				lat, lon, alt = None, None, None
+	except pynmea2.ChecksumError:
+		print ("GPS::MAIN::loop_gps_lines::checksumerror:", e)
+		return 0
 
 	except Exception as e:
-		print ("MAIN::READ_LINE::unknown exception", e)
-		ser.close()
+		print ("GPS::MAIN::loop_gps_lines::exception:", e)
 		return 1
-
-	finally:
-		ser.close()
-		return 0
 
 
 if __name__ == "__main__":
