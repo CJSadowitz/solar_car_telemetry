fde65b96d30a36c79be3b37420a0cc55ebdb0102
colin sadowitz <sadowitzc@gmail.com>
2025-06-23 09:39:21 -0400

    Added overcurrent protection and removed front end service

--- web_server/main.py
+++ web_server/main.py
@@ -1,24 +1,15 @@
-from flask import Flask, render_template
-from flask_cors import CORS
+from flask import Flask
 import get
 
 app = Flask(__name__, static_folder="static")
 CORS(app)
-
-@app.route("/home")
-def home():
-	return render_template("index.html")
-
-@app.route("/recent_data")
-def recent_data():
-	return render_template("recent_data.html")
 
 @app.route("/get_data", methods=["GET"])
 def get_data():
 	return { "body": get.get_data() }
 
 @app.route("/get_graph_data", methods=["GET"])
-def get_graph_Data():
+def get_graph_data():
 	return { "body": get.get_graph_data() }
 
 if __name__ == "__main__":
