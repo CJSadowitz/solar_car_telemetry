55ee4fc4bc893a62e16329652d551a591afa6db5
colin sadowitz <sadowitzc@gmail.com>
2025-06-24 14:29:59 -0400

    logging for speed; need to fix pico code

--- speed/main.py
+++ speed/main.py
@@ -3,15 +3,16 @@ import serial_helper
 import write_db
 
 def main():
-	port = "/dev/serial0"
+	port = "/dev/ttyACM0"
 	ser = None
-	while ser != None:
+	while ser == None:
 		ser = serial_helper.get_serial(port)
 
 	while True:
 		line = serial_helper.get_line(ser)
 		if line != None:
-			write_db.save_data(line)
+			print (line)
+			# write_db.save_data(line)
 
 if __name__ == "__main__":
 	main()
--- speed/serial_helper.py
+++ speed/serial_helper.py
@@ -1,7 +1,7 @@
 import serial
 
 def get_serial(port):
-	ser = serial.Serial(port, baudrate=9600, timeout=1)
+	ser = serial.Serial(port, baudrate=115200, timeout=1)
 	if ser == None:
 		print ("SPEED::serial_helper::get_serial:serial port not found")
 	return ser
