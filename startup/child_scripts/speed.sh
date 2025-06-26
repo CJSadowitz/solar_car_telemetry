d3c5c4412b7e89aa4709589c19ef7c9624b267da
colin sadowitz <sadowitzc@gmail.com>
2025-06-26 17:16:14 -0400

    Added speed to startup

---
+++ startup/child_scripts/speed.sh
@@ -0,0 +1,4 @@
+echo "SPEED.sh"
+source ../speed/venv/bin/activate
+python3 ../speed/main.py
+echo "SPEED.sh::failed"
--- startup/master_startup.sh
+++ startup/master_startup.sh
@@ -5,9 +5,9 @@ cd "$(dirname "$0")"
 bash child_scripts/database.sh
 bash child_scripts/can.sh &
 bash child_scripts/lte.sh &
+bash child_scripts/speed.sh &
 # bash child_scripts/gps.sh &
 bash child_scripts/pi_monitor.sh &
-bash child_scripts/cloudflare.sh &
+bash child_scripts/cloudflare.sh & # NEVER COMMENT THIS OUT
 bash child_scripts/flask.sh &
-# bash child_scripts/profinity.sh &
 bash child_scripts/gui.sh &
