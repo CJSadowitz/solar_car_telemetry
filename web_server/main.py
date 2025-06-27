6e847d5b57fc50a11ce93dc6a45c7517ded7ec9b
colin sadowitz <sadowitzc@gmail.com>
2025-06-27 09:46:03 -0400

    refactored for postgres db and split up get.py file

--- web_server/main.py
+++ web_server/main.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
 import asyncio
-import get
+import get_data_page
+import get_graph_page
 
 app = Flask(__name__, static_folder="static")
 CORS(app)
 
 @app.route("/get_data", methods=["GET"])
 def get_data():
-	return { "body": asyncio.run(get.get_data()) }
+	return { "body": asyncio.run(get_data_page.get_data()) }
 
 @app.route("/get_graph_data", methods=["GET"])
 def get_graph_data():
-	return { "body": get.get_graph_data() }
+	return { "body": asyncio.run(get_graph_page.get_graph_data()) }
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8008)
